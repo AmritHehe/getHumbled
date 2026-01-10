793de79909407ed2a14ceabfd7a2f1abf6e2adbb
AmritHehe <hopesalive1506@gmail.com>
2026-01-10 19:42:49 +0530

    feat(validation): added zod schema for contest and create question

---
+++ apps/api/src/validators/contest.schema.ts
@@ -0,0 +1,27 @@
+
+import z from "zod"
+
+export const ContestSchema = z.object({
+    title : z.string().min(10).max(100),
+    discription : z.string().min(10).max(2000),
+    type : z.enum(["DSA" , "DEV"]),
+    status : z.enum([  "UPCOMING" ,"LIVE" ,"CLOSED"]),
+    StartTime : z.date(), 
+})
+
+export const MCQSchema = z.object({
+    createdBy : z.string(),
+    question : z.string(),
+    Soltion : z.enum(["A", "B", "C", "D"]),
+    createdAt : z.date(),
+    points : z.int(),
+    avgTTinMins : z.int().optional()
+    
+})
+
+export const CodeSchema = z.object({
+    createdBy : z.object() ,
+    question : z.string() , 
+    Solution : z.string() , 
+    avgTTinMins : z.int().optional()
+}) 
