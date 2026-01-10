ef22548a2cb5adafc4b57f6851ff162f2ecd1e83
AmritHehe <hopesalive1506@gmail.com>
2026-01-10 16:12:43 +0530

    feat(validation): add zod schemas for auth requests

--- apps/api/src/validators/user.schema.ts
+++ apps/api/src/validators/user.schema.ts
@@ -1,8 +1,12 @@
 import { password } from "bun"
 import z from "zod" 
 
-export const UserSchema = z.object({
+export const UserSignUpSchema = z.object({
     username : z.string(),
+    email : z.email(), 
+    password : z.string()
+})
+export const UserSignInSchema = z.object({
     email : z.email(), 
     password : z.string()
 })
