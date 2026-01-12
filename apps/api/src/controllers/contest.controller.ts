9064058dc20365c84b5f82751f03179bc212a770
AmritHehe <hopesalive1506@gmail.com>
2026-01-12 23:56:00 +0530

    feat(Contest): added Getcontest and get all contests logic

--- apps/api/src/controllers/contest.controller.ts
+++ apps/api/src/controllers/contest.controller.ts
@@ -1,6 +1,8 @@
 import type { Request , Response } from "express";
-import { ContestSchema, MCQSchema } from "../validators/contest.schema";
+import { ContestSchema, GetAllContestSchema, GetContestSchema, MCQSchema } from "../validators/contest.schema";
 import { prisma } from "@repo/database";
+import { success } from "zod";
+import { memo } from "react";
 
 
 export async function CreateContest(req : Request  , res : Response){ 
@@ -53,6 +55,8 @@ export async function CreateContest(req : Request  , res : Response){
 }
 export async function CreateMcqQuestion(req : Request  , res : Response){ 
     //ned contest id here 
+    //either get contest id from params or get contest id in body
+
     const userId = req.userId ; 
     if(!userId){ 
         return res.status(410).json({
@@ -75,11 +79,15 @@ export async function CreateMcqQuestion(req : Request  , res : Response){
         })
     }
     try { 
-        // const result = await  prisma.mCQ.create({
-        //     data : { 
-        //         c
-        //     }
-        // })
+        const result = await  prisma.mCQ.create({
+            data : { 
+                contestId : data.contestId , 
+                question : data.question , 
+                Solution : data.Soltion,
+                avgTTinMins : data.avgTTinMins,
+                points : 10
+            }
+        })
     }
     catch(e){ 
         return res.status(500).json({
@@ -90,9 +98,69 @@ export async function CreateMcqQuestion(req : Request  , res : Response){
 }  
 
 export async function GetContest(req : Request  , res : Response){ 
-    
+    const {data , success} = GetContestSchema.safeParse(req.body)
+    if(!success){ 
+        return res.status(400).json({
+            sucess : false , 
+            message : "invalid schema"
+        })
+    }
+    const contestId = data.contestId ; 
+    try { 
+        let contest = await prisma.contests.findUnique({
+            where : { 
+                id : contestId 
+            }
+        })
+        if(contest == null){ 
+            return res.status(404).json({
+                success : false , 
+                error : "contest not found"
+            })
+        }
+        return res.status(200).json({
+            success : true , 
+            data : contest
+        })
+    }
+    catch(e){ 
+        return res.status(500).json({
+            success : false ,
+            error : "database is down , error " + e
+        })
+    }
 }
 
 export async function GetAllContest(req : Request  , res : Response){ 
-    
+    const {data , success } = GetAllContestSchema.safeParse(req.body)
+    if(!success){ 
+        return res.status(400).json({
+            success : false , 
+            error : "Invalid Schema"
+        })
+    }
+    try { 
+        let allContest ;
+        if(data.status == "ALL"){
+            allContest = await prisma.contests.findMany({})
+        }
+        else { 
+            allContest = await prisma.contests.findMany({
+                where : {
+                    status : data.status
+                }
+            })
+        }
+         
+        return res.status(200).json({
+            success : true , 
+            data : allContest
+        })
+    }
+    catch(e){ 
+        res.status(500).json({
+            success : false , 
+            message : "coudnt find the database"
+        })
+    }
 }
