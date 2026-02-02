eea6d85315c8268cbd4512d28ec676999a994637
AmritHehe <hopesalive1506@gmail.com>
2026-02-02 20:06:54 +0530

    fix(cors): added localhost port in origin

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -7,7 +7,7 @@ import cors from  "cors"
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:3000" , "https://get-humbled-web.vercel.app" , "https://skillup.amrithehe.com"], // allowed frontend origins
+  origin: ["http://localhost:3001" , "https://get-humbled-web.vercel.app" , "https://skillup.amrithehe.com"], // allowed frontend origins
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
